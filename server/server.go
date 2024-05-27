38d3d34035b83c3d8c5278d767142e993c892045
sergo <154074169+serbebra@users.noreply.github.com>
2024-05-27 03:34:22 +0200

    Update server.go
    Signed-off-by: sergo <154074169+serbebra@users.noreply.github.com>

--- server/server.go
+++ server/server.go
@@ -2,7 +2,6 @@ package server
 
 import (
 	"context"
-	"errors"
 	"fmt"
 	"net/http"
 	"sync"
@@ -11,10 +10,8 @@ import (
 	"github.com/berachain/offchain-sdk/log"
 )
 
-// 10 seconds is a stable default.
 const defaultReadHeaderTimeout = 10 * time.Second
 
-// Handler is a handler.
 type Handler struct {
 	Path    string
 	Handler http.Handler
@@ -22,67 +19,56 @@ type Handler struct {
 
 type Middleware func(http.Handler) http.Handler
 
-// Server is a server, that currently only supports HTTP.
 type Server struct {
-	cfg    *Config
-	logger log.Logger
-
-	mux    *http.ServeMux
-	srv    *http.Server
-	closer sync.Once
-
+	cfg         *Config
+	logger      log.Logger
+	mux         *http.ServeMux
+	srv         *http.Server
+	closer      sync.Once
 	middlewares []Middleware
 }
 
-// New creates a new server.
 func New(cfg *Config, logger log.Logger, middlewares ...Middleware) *Server {
-	return &Server{
+	server := &Server{
 		cfg:         cfg,
 		logger:      logger,
 		mux:         http.NewServeMux(),
 		middlewares: middlewares,
 	}
+	return server
 }
 
-// RegisterHandler registers a handler.
 func (s *Server) RegisterHandler(h *Handler) {
-	s.mux.Handle(h.Path, h.Handler)
+	chain := h.Handler
+	for i := len(s.middlewares) - 1; i >= 0; i-- {
+		chain = s.middlewares[i](chain)
+	}
+	s.mux.Handle(h.Path, chain)
 }
 
-// RegisterMiddleware registers a middleware.
 func (s *Server) RegisterMiddleware(m Middleware) {
 	s.middlewares = append(s.middlewares, m)
 }
 
-// applyMiddlewares applies the middlewares to the server in reverse order,
-// so that the first middleware is the outermost one.
-func (s *Server) applyMiddlewares() http.Handler {
-	var h http.Handler = s.mux
-	for i := len(s.middlewares) - 1; i >= 0; i-- {
-		h = s.middlewares[i](h)
-	}
-	return h
-}
-
-// Start starts the server. It is blocking so must run in a go-routine.
 func (s *Server) Start(ctx context.Context) {
 	s.srv = &http.Server{
 		Addr:              fmt.Sprintf("%s:%d", s.cfg.HTTP.Host, s.cfg.HTTP.Port),
-		Handler:           s.applyMiddlewares(),
+		Handler:           s.mux,
 		ReadHeaderTimeout: defaultReadHeaderTimeout,
 	}
 
-	if err := s.srv.ListenAndServe(); !errors.Is(err, http.ErrServerClosed) {
+	go func() {
+		<-ctx.Done()
+		s.Stop()
+	}()
+
+	if err := s.srv.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
 		s.logger.Error("HTTP server errored", "err", err)
 	} else {
 		s.logger.Info("HTTP server closed")
 	}
-
-	<-ctx.Done()
-	s.Stop()
 }
 
-// Stop stops the server.
 func (s *Server) Stop() {
 	s.closer.Do(func() {
 		if err := s.srv.Close(); err != nil {
